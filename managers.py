c376e549ee626d1e45b2ea7e9f48253a27003dfe
Fabian Fonseca <fhfonsecaa@gmail.com>
2021-04-14 12:54:58 +0200

    templates

---
+++ managers.py
@@ -0,0 +1,43 @@
+class WaporAPIManager:
+    def __init__(self, ):
+        pass
+    
+    def connectWapor(self):
+        pass
+
+    def disconnectWapor(self):
+        pass
+
+    def login(self):
+        pass
+
+    def query(self):
+        pass
+
+    def isConnected(self):
+        pass
+
+class FileManager:
+    def __init__(self, ):
+        pass
+
+    def checkWS():
+        pass
+
+    def createWS():
+        pass
+
+    def 
+
+
+
+class CanvasManager:
+    def __init__(self, ):
+
+
+class WindowManager:
+    def __init__(self, ):
+
+class IndicatorCalculator:
+    def __init__(self, ):
+        pass
